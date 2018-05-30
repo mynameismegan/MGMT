696eb0088c09f43fb0b0e2ae802085b380bf348b
mynameismegan <egan@ualberta.ca>
2018-05-30 12:01:34 -0700

    pushing website

--- assets/javascripts/functions.js
+++ assets/javascripts/functions.js
@@ -6,31 +6,26 @@ function getSrc(source)
     document.getElementById('active-video').click();
 }
 
-// When the user scrolls the page, execute myFunction
-// document.getElementById("letsScroll").onscroll = function() {
-//     tourFunction()
-//     };
-//
-// // Get the navbar
-// var navbarTour = document.getElementById("navbar-menu");
-//
-// // Get the offset position of the navbar
-// var stickyTour = navbarTour.offsetTop;
-//
-// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-// function tourFunction() {
-//   if (document.getElementById("letsScroll").scrollTop >= stickyTour) {
-//     navbarTour.classList.add("sticky")
-//     var stickyClass = document.getElementsByClassName("sticky")[0]
-//     while (document.getElementById("letsScroll").scrollTop >= stickyTour) {
-//       stickyClass.style.top = document.getElementById("letsScroll").scrollTop
-//     }
-//     console.log(stickyTour, document.getElementById("letsScroll").scrollTop)
-//   } else {
-//     navbar.classList.remove("sticky");
-//   }
-// }
 
+function setActive(elem)
+{
+  document.getElementsByClassName("active")[0].classList.remove("active")
+  if (elem == 'all'){
+    document.getElementsByClassName("fltr-controls")[0].classList.add("active")
+  }
+  else if (elem == '1') {
+    document.getElementsByClassName("fltr-controls")[1].classList.add("active")
+  }
+  else if (elem == '2') {
+    document.getElementsByClassName("fltr-controls")[2].classList.add("active")
+  }
+  else if (elem == '3') {
+    document.getElementsByClassName("fltr-controls")[3].classList.add("active")
+  }
+  else if (elem == '4') {
+    document.getElementsByClassName("fltr-controls")[4].classList.add("active")
+  }
+}
 
 // When the user scrolls the page, execute myFunction
 window.onscroll = function() {myFunction()};
@@ -46,14 +41,18 @@ function myFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
     content.style.paddingTop = "60px"
-    if (content.children[1].classList.contains("push-down")){
+    if (content.children.length > 1){
+      if (content.children[1].classList.contains("push-down")){
       document.getElementsByClassName("push-down")[0].style.paddingTop = "60px"
+      }
     }
   } else {
     navbar.classList.remove("sticky");
     content.style.paddingTop = "0px"
-    if (content.children[1].classList.contains("push-down")){
+    if (content.children.length > 1){
+      if (content.children[1].classList.contains("push-down")){
       document.getElementsByClassName("push-down")[0].style.paddingTop = "0px"
+      }
     }
   }
 }
